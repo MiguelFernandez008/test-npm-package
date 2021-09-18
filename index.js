1be4beaf5c1bb3f5652e7a57d82de653b40a3221
Miguel Fernández <miguel.fernandez008@gmail.com>
2021-09-18 14:48:35 +0200

    :sparkles: Test package
    [General]
    -> Added: index.js
    -> Added: package.json

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Test function that shows a message in console.
+ * 
+ * @param {String} message Message to show in console.
+ */
+export const printTextmessage = function(message) {
+    console.log(message);
+}
