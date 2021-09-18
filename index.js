ded71ad90a7b9fba9040212f7405390bebd4d50a
Miguel Fernández <miguel.fernandez008@gmail.com>
2021-09-18 18:23:48 +0200

    :sparkles: Finish
    [General]
    -> Changed: Update cooments.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Test function that shows a message in console.
  * 
- * @param {String} message Message to show in console.
+ * @param {String} message Message to show.
  */
 export const printTextmessage = function(message) {
     console.log(message);
